f1f963bfcd7dc69c73d24d792fb8ae2f51ca5c91
D. Moschokarfi <despinamx16@gmail.com>
2026-06-08 20:24:16 +0300

    feat: embedding service created

---
+++ app/services/embedding_service.py
@@ -0,0 +1,20 @@
+from langchain_openai import OpenAIEmbeddings
+
+EMBED_MODEL = "text-embedding-3-small"
+
+class EmbeddingService:
+    _embedder = OpenAIEmbeddings(model=EMBED_MODEL)
+
+    @staticmethod
+    def embed_text(text: str) -> list[float]:
+        """
+        Converts one text string into one embedding vector.
+        """
+        return EmbeddingService._embedder.embed_query(text)
+    
+    @staticmethod
+    def embed_texts(texts: list[str]) -> list[list[float]]:
+        """
+        Converts multiple text chunks into embedding vectors.
+        """
+        return EmbeddingService._embedder.embed_documents(texts)
