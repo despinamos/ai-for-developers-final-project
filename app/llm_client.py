7212b550920bd3e0037600121d63b0f23227800c
D. Moschokarfi <despinamx16@gmail.com>
2026-06-16 15:10:03 +0300

    feat: stream chat function created

--- app/llm_client.py
+++ app/llm_client.py
@@ -1,5 +1,5 @@
 """
-LLM Client with simple chat method.
+LLM Client with simple chat method and stream chat method.
 """
 
 import os
@@ -10,12 +10,28 @@ load_dotenv()
 
 client = OpenAI()
 
+llm_model = "gpt-4o-mini"
+llm_temp = 0.3
+
 class LLMClient:
     @staticmethod 
-    def chat(messages, temperature=0.3): 
+    def chat(messages, temperature=llm_temp): 
         response = client.chat.completions.create( 
-            model="gpt-4o-mini", 
+            model=llm_model, 
             messages=messages, 
             temperature=temperature 
         ) 
         return response.choices[0].message.content
+    
+    @staticmethod
+    def stream_chat(messages, temperature=llm_temp):
+        stream = client.chat.completions.create(
+            model=llm_model,
+            messages=messages,
+            stream=True
+        )
+
+        for chunk in stream:
+            delta = chunk.choices[0].delta.content
+            if delta:
+                yield delta
