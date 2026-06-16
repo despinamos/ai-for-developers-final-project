67012a3eff8b159706de59b58fa97c4f986bc59f
D. Moschokarfi <despinamx16@gmail.com>
2026-06-16 13:00:05 +0300

    fix: moved explain improve and review schemas in correct folder

---
+++ app/schemas/explain.py
@@ -0,0 +1,29 @@
+"""
+Request and response models for code explainator AI function.
+"""
+
+from typing import Optional
+from pydantic import BaseModel, Field
+from enum import Enum
+
+class UserSkillLevel(str, Enum):
+    """Possible user skill levels"""
+    BEGINNER = "beginner"
+    INTERMEDIATE = "intermediate"
+    ADVANCED = "advanced"
+
+class CodeExplainRequest(BaseModel):
+    """Request model for code explanator"""
+    system_prompt: str
+    code: str = Field(..., min_length=10, description="The code to be explained.")
+    language: Optional[str] = Field(default="python", description="Programming language")
+    level: Optional[UserSkillLevel] = Field(
+        default=UserSkillLevel.BEGINNER,
+        description="User level skill."
+    ) 
+
+class CodeExplainResponse(BaseModel):
+    """Response model for code explanator"""
+    explanation: str
+    language: str = "python"
+    level: UserSkillLevel = "beginner"
---
+++ app/schemas/improve.py
@@ -0,0 +1,29 @@
+"""
+Request and response models for code improving AI function.
+"""
+
+from typing import Optional
+from pydantic import BaseModel, Field
+from enum import Enum
+
+class UserSkillLevel(str, Enum):
+    """Possible user skill levels"""
+    BEGINNER = "beginner"
+    INTERMEDIATE = "intermediate"
+    ADVANCED = "advanced"
+
+class CodeImproveRequest(BaseModel):
+    """Request model for code improver"""
+    system_prompt: str
+    code: str = Field(..., min_length=10, description="The code to be explained.")
+    language: Optional[str] = Field(default="python", description="Programming language")
+    level: Optional[UserSkillLevel] = Field(
+        default=UserSkillLevel.BEGINNER,
+        description="User level skill."
+    ) 
+
+class CodeImproveResponse(BaseModel):
+    """Response model for code improver"""
+    improve: str
+    language: str = "python"
+    level: UserSkillLevel = "beginner"
